932870db1c10efe52b3d872685130f5c2741d9e1
Alexander Hare <a-hare@onu.edu>
2023-02-14 12:53:02 -0500

    Full Square adj. Matrix, input still lower triangular

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,6 @@
 
 #include <vector>;
+#include <string>;
 #include <iostream>;
 using namespace std;
 
@@ -18,14 +19,20 @@ class Graph
 		//Increase adjascency Matrix by adding one to each vector
 		void createNode()
 		{
+			for (int i = 0; i < nodes; i++)
+			{
+				adj[i].push_back(0);
+			}
 			nodes = nodes + 1;
-			adj.push_back(vector<int>(nodes));
+			adj.push_back(vector<int>(nodes,0));
+			
 		}
 
 		// Modify adjascency matrix by adding weights to existing connections
 		void createEdge(int a, int b, int weight)
 		{
 			adj[a][b] = weight;
+			adj[b][a] = weight;
 		}
 	
 	public:
@@ -48,7 +55,7 @@ class Graph
 			{
 				cout << "|";
 			
-				for (int j = 0; j <= i; j++)
+				for (int j = 0; j < nodes; j++)
 				{
 					cout << adj[i][j] << "|";
 				}
@@ -62,29 +69,49 @@ int main()
 	Graph g;
 	vector<vector<int>> in;
 	int n;
+	string user;
 
 	// Request end number of nodes from user
 	cout << "Number of Nodes: ";
  	cin >> n;
 	in.resize(n, vector<int>(n));
-	// request adjascency matrix from user, in nxn format
-	cout << endl << "Please enter the adjascency matrix of the graph in row order: ";
+	// request adjascency matrix from user, in lower triangle format
+	// users will input list enclosed in brackets as prompted with entries separated by comas
+
+	
+	cout << endl << "You will now enter each row of your adjacency matrix," << endl << "please seperate entries with commas and enclose each list in square brackets" << endl << endl;
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
+		int j = 0;
+		int count = -1;
+		if (i == 0)
+			cout << "Please enter row " << i + 1 << " of the lower triangular matrix" << endl << "You should input " << i + 1 << " entry" << endl << endl;
+		else
+			cout << endl << "Please enter row " << i + 1 << " of the lower triangular matrix" << endl << "You should input " << i + 1 << " comma seperated entries" << endl << endl;
+
+		cin >> user;
+
+		for (int k = 1; k < user.length() ; k++)
 		{
-			cin >> in[i][j];
+			count++;
+			if (user.at(k) == ',' || user.at(k) ==']')
+			{
+				in[i][j] = stoi(user.substr(k-count,count));
+				j++;
+				count = -1;
+			}
 		}
 	}
+
 	
-	// Add vectors to adjascency matrix from input 2d vector 'in' (row by row)
+	// Populates adjascency matrix from input 2d vector 'in' (row by row)
 	for (int i = 0; i < n; i++)
 	{
-		cout << "i = " << i << endl;
 		g.addNode(in[i]);
 	}
 	
 	// Output visual display of adjascency matrix
+	cout << endl <<  "The adjacency matrix of the graph input: " << endl << endl;
 	g.disp();
 
 }
